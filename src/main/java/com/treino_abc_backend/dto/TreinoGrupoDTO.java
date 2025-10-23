38eff787cd42f66a4f897b40d43f2d6c7247c541
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2025-10-23 18:47:32 -0300

    fix: ajuste teste

--- src/main/java/com/treino_abc_backend/dto/TreinoGrupoDTO.java
+++ src/main/java/com/treino_abc_backend/dto/TreinoGrupoDTO.java
@@ -4,16 +4,37 @@ import java.util.List;
 import java.util.UUID;
 
 public class TreinoGrupoDTO {
+
     private UUID id;
+    private UUID alunoId;
     private String nome;
     private List<TreinoExercicioDTO> exercicios;
 
+    //  Construtor padrão
+    public TreinoGrupoDTO() {}
+
+    //  Construtor completo
+    public TreinoGrupoDTO(UUID id, UUID alunoId, String nome) {
+        this.id = id;
+        this.alunoId = alunoId;
+        this.nome = nome;
+    }
+
+    //  Getters e Setters
     public UUID getId() {
         return id;
     }
 
     public void setId(UUID id) {
         this.id = id;
+    }
+
+    public UUID getAlunoId() {
+        return alunoId;
+    }
+
+    public void setAlunoId(UUID alunoId) {
+        this.alunoId = alunoId;
     }
 
     public String getNome() {
@@ -31,6 +52,4 @@ public class TreinoGrupoDTO {
     public void setExercicios(List<TreinoExercicioDTO> exercicios) {
         this.exercicios = exercicios;
     }
-
-    // Getters e Setters
 }
