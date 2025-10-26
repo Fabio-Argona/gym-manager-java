aa44aa7c7ace8148efdbcec6400732b8d0d79f25
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2025-10-26 18:46:05 -0300

    feat: ajuste controler

---
+++ src/main/java/com/treino_abc_backend/dto/ExercicioEdicaoDTO.java
@@ -0,0 +1,89 @@
+package com.treino_abc_backend.dto;
+
+import java.util.UUID;
+
+public class ExercicioEdicaoDTO {
+    private String nome;
+    private UUID grupoId;
+    private String grupoMuscular;
+    private int series;
+    private int repMin;
+    private int repMax;
+    private Double pesoInicial;
+    private String observacao;
+    private boolean ativo;
+
+    // Getters e setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public UUID getGrupoId() {
+        return grupoId;
+    }
+
+    public void setGrupoId(UUID grupoId) {
+        this.grupoId = grupoId;
+    }
+
+    public String getGrupoMuscular() {
+        return grupoMuscular;
+    }
+
+    public void setGrupoMuscular(String grupoMuscular) {
+        this.grupoMuscular = grupoMuscular;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public int getRepMin() {
+        return repMin;
+    }
+
+    public void setRepMin(int repMin) {
+        this.repMin = repMin;
+    }
+
+    public int getRepMax() {
+        return repMax;
+    }
+
+    public void setRepMax(int repMax) {
+        this.repMax = repMax;
+    }
+
+    public Double getPesoInicial() {
+        return pesoInicial;
+    }
+
+    public void setPesoInicial(Double pesoInicial) {
+        this.pesoInicial = pesoInicial;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+}
